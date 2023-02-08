a9aa1d48db7123cdf41df7cf6d1822ec8fb6b654
Andrew Godwin <andrew@aeracode.org>
2023-02-08 16:37:22 -0700

    Get calendar bits working

---
+++ caldisplay/server/README.md
@@ -0,0 +1,7 @@
+# CalDisplay Server
+
+Configure using an environment variable called `CALENDARS` in the format:
+
+`NAME:https://url|NAME:https://url`
+
+And a value called `API_KEY` that matches the `key` URL parameter your device sends.
--- caldisplay/server/server.py
+++ caldisplay/server/server.py
@@ -1,4 +1,8 @@
 import arrow
+import datetime
+import os
+import requests
+from icalevents import icalevents
 from io import BytesIO
 from PIL import Image, ImageDraw, ImageFont
 from flask import Flask, send_file, request
@@ -8,18 +12,28 @@ app = Flask(__name__)
 
 class Colours:
     BLACK = 0
-    DARK_GREY = 63
+    DARK_GREY = 65
     GREY = 127
     LIGHT_GREY = 191
+    VERY_LIGHT_GREY = 223
     WHITE = 255
 
 
 @app.route("/")
 def index():
-    # Get API key
+    # Get API key and PNG mode
     png_mode = request.args.get("png")
+    api_key = request.args.get("key")
+    if os.environ.get("API_KEY"):
+        if api_key != os.environ["API_KEY"]:
+            return "Invalid API key", 403
+    # Get calendar URLs
+    calendar_urls = {}
+    for entry in os.environ["CALENDARS"].split("|"):
+        name, url = entry.split(":", 1)
+        calendar_urls[name.strip()] = url.strip()
     # Render
-    image = CalendarRenderer({}).render()
+    image = CalendarRenderer(calendar_urls).render()
     output = BytesIO()
     if png_mode:
         image.save(output, "PNG")
@@ -44,6 +58,20 @@ class CalendarRenderer:
         self.size = (1100, 725)
         self.timezone = timezone
         self.now: arrow.Arrow = arrow.utcnow().to(self.timezone)
+        self.tomorrow: arrow.Arrow = self.now.shift(days=1).floor("day")
+        self.events = []
+        for name, url in self.calendar_urls.items():
+            print("Downloading " + url)
+            data = requests.get(url).text.encode("utf8")
+            print("Parsing " + url)
+            for event in icalevents.events(
+                string_content=data,
+                start=self.now.floor("day"),
+                end=self.now.shift(days=4),
+            ):
+                event.source = name
+                self.events.append(event)
+        self.events.sort(key=lambda e: e.start)
         self.fonts = {}
 
     def render(self) -> Image.Image:
@@ -64,17 +92,105 @@ class CalendarRenderer:
         self.draw_text(
             (dw + 40, 17), self.now.strftime("%B %Y"), Colours.GREY, ("medium", 30)
         )
-        # Draw some test rects
-        self.draw.rectangle((0, 200, 200, 230), Colours.BLACK)
-        self.draw.rectangle((0, 230, 200, 260), Colours.DARK_GREY)
-        self.draw.rectangle((0, 260, 200, 290), Colours.GREY)
-        self.draw.rectangle((0, 290, 200, 320), Colours.LIGHT_GREY)
+        # Draw the columns
+        self.draw_cal_column(0, 80, "Today", self.now, self.tomorrow)
+        self.draw_cal_column(
+            350, 80, "Tomorrow", self.tomorrow, self.tomorrow.shift(days=1)
+        )
+        self.draw_cal_column(
+            700,
+            80,
+            self.tomorrow.shift(days=1).strftime("%A"),
+            self.tomorrow.shift(days=1),
+            self.tomorrow.shift(days=2),
+        )
         return self.image
+
+    def draw_cal_column(
+        self, x: int, y: int, title: str, start_time, end_time, width=300
+    ):
+        self.draw.rounded_rectangle(
+            (x, y, x + width, y + 30), 5, fill=Colours.DARK_GREY
+        )
+        self.draw.rectangle((x, y + 15, x + width, y + 30), fill=Colours.DARK_GREY)
+        self.draw_text((x + 10, y + 3), title, Colours.WHITE, ("bold", 20))
+        top = y + 40
+        last_end: datetime.datetime | None = None
+        for event in self.events:
+            # Skip events outside the window
+            if event.end < start_time or event.start > end_time:
+                continue
+            # Skip events that were declined
+            attendee = event.attendee
+            if isinstance(attendee, list):
+                for person in attendee:
+                    if "godwin" in person.lower():
+                        attendee = person
+            if (
+                hasattr(attendee, "params")
+                and attendee.params.get("PARTSTAT", "").lower() == "declined"
+            ):
+                continue
+            # Skip lunch and generic busy events
+            if event.summary.lower() in ["busy", "lunch", "out", "ooo"]:
+                continue
+            # Maybe add a gap indicator
+            if last_end:
+                gap = event.start - last_end
+                if gap.total_seconds() > 300:
+                    top = self.draw_cal_gap(
+                        x, top, width, self.format_duration(gap) + " gap"
+                    )
+            # Draw the actual event
+            top = self.draw_cal_event(
+                x,
+                top,
+                width,
+                self.format_short_time(event.start),
+                self.format_duration(event.end - event.start, short=True),
+                event.summary,
+            )
+            last_end = event.end
+        # If there were no events, show a placeholder
+        if top == y + 40:
+            self.draw_cal_gap(x, top + 10, width, "No events")
+
+    def draw_cal_event(
+        self, x: int, y: int, width: int, time: str, duration: str, title: str
+    ) -> int:
+        self.draw_text((x, y), time, Colours.BLACK, ("bold", 20))
+        self.draw.rounded_rectangle(
+            (x + width - 50, y, x + width, y + 20), 3, Colours.GREY
+        )
+        self.draw_text(
+            (x + width - 25, y),
+            duration,
+            Colours.WHITE,
+            ("bold", 15),
+            align="centre",
+        )
+        self.draw_text((x, y + 30), title, Colours.BLACK, ("light", 20))
+        return y + 65
+
+    def draw_cal_gap(self, x: int, y: int, width: int, duration: str) -> int:
+        self.draw.rounded_rectangle(
+            (x + 20, y, x + width - 20, y + 20), 3, Colours.LIGHT_GREY
+        )
+        self.draw_text(
+            (x + (width / 2), y),
+            duration,
+            Colours.WHITE,
+            ("bold", 15),
+            align="centre",
+        )
+        return y + 35
 
     def draw_text(self, pos, text, colour, font, align="left") -> tuple[int, int]:
         """
         Draws text and returns its size
         """
+        # Remove any emoji
+        text = ("".join(c for c in text if ord(c) < 255)).strip()
         # Get font
         if font not in self.fonts:
             self.fonts[font] = ImageFont.truetype(
@@ -90,3 +206,29 @@ class CalendarRenderer:
             x -= size[0] / 2
         self.draw.text((x, y), str(text), fill=colour, font=self.fonts[font])
         return size
+
+    def format_short_time(self, time: datetime.datetime) -> str:
+        if time.minute == 0:
+            return arrow.get(time).to(self.timezone).strftime("%-I%p").lower()
+        else:
+            return arrow.get(time).to(self.timezone).strftime("%-I:%M%p").lower()
+
+    def format_duration(self, duration: datetime.timedelta, short=False) -> str:
+        seconds = duration.total_seconds()
+        if seconds < 3600:
+            value = seconds // 60
+            suffix = "min"
+        elif seconds < 86400:
+            value = seconds // 3600
+            suffix = "hour"
+        else:
+            value = seconds // 86400
+            suffix = "day"
+        if int(value) == value:
+            value_str = f"{int(value)}"
+        else:
+            value_str = f"{value:.1f}"
+        if short:
+            return value_str + suffix[0]
+        else:
+            return value_str + " " + suffix
